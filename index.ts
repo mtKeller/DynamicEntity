49e2782e0e1a6730663dee878d72b7ffa843f436
Micah <micah.t.keller@gmail.com>
2018-12-22 13:39:19 -0800

    Removed test scripts

--- index.ts
+++ index.ts
@@ -1,42 +1,42 @@
-import { DynamicEntity } from './src/DynamicEntity.class';
+// import { DynamicEntity } from './src/DynamicEntity.class';
 
 export { DynamicEntity } from './src/DynamicEntity.class';
 
-let demoEntity: DynamicEntity<any> = new DynamicEntity<any>([0,1,2,3,4,5]);
+// let demoEntity: DynamicEntity<any> = new DynamicEntity<any>([0,1,2,3,4,5]);
 
-console.log("Outputs: ", (demoEntity));
-// Prints: { '0': 1, '1': 2, '2': 3, '3': 4, '4': 5 }
+// console.log("Outputs: ", (demoEntity));
+// // Prints: { '0': 1, '1': 2, '2': 3, '3': 4, '4': 5 }
 
-console.log("Outputs: ", demoEntity[3]);
-// Prints: 4
-demoEntity = demoEntity.append('ketchup');
-demoEntity = demoEntity.filter((entry) => entry !== 4);
+// console.log("Outputs: ", demoEntity[3]);
+// // Prints: 4
+// demoEntity = demoEntity.append('ketchup');
+// demoEntity = demoEntity.filter((entry) => entry !== 4);
 
-console.log("Outputs: ", demoEntity[6]);
+// console.log("Outputs: ", demoEntity[6]);
 
 
-demoEntity = demoEntity.append('sriracha');
-demoEntity = demoEntity.filter((entry) => entry !== 'ketchup');
-demoEntity = demoEntity.append(123);
+// demoEntity = demoEntity.append('sriracha');
+// demoEntity = demoEntity.filter((entry) => entry !== 'ketchup');
+// demoEntity = demoEntity.append(123);
 
-console.log("Outputs: ", demoEntity);
+// console.log("Outputs: ", demoEntity);
 
-console.log("Outputs: ", demoEntity[4]);
+// console.log("Outputs: ", demoEntity[4]);
 
-demoEntity = demoEntity.map((entry) => {
-    if (typeof entry === "number") {
-        return entry * 2;
-    } else {
-        return (entry[0] as string).toUpperCase() + (entry as string).slice(1) + " so tasty!"
-    }
-})
+// demoEntity = demoEntity.map((entry) => {
+//     if (typeof entry === "number") {
+//         return entry * 2;
+//     } else {
+//         return (entry[0] as string).toUpperCase() + (entry as string).slice(1) + " so tasty!"
+//     }
+// })
 
-let held;
-[ demoEntity, held] = demoEntity.borrow((val) => 10 === val);
+// let held;
+// [ demoEntity, held] = demoEntity.borrow((val) => 10 === val);
 
-console.log("Outputs: ", held);
-console.log("Outputs: ", demoEntity);
+// console.log("Outputs: ", held);
+// console.log("Outputs: ", demoEntity);
 
-demoEntity = demoEntity.return(held);
+// demoEntity = demoEntity.return(held);
 
-console.log("Outputs: ", demoEntity);
+// console.log("Outputs: ", demoEntity);
